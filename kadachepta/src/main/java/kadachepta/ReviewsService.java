4f3442b2c4171216c1912ffb92f260222b91eac7
Abhilash Juluri <ajuluri@gmail.com>
2017-06-28 22:18:11 -0400

    Artist & Story Reviews skeleton, data structure

--- kadachepta/src/main/java/kadachepta/ReviewsService.java
+++ kadachepta/src/main/java/kadachepta/ReviewsService.java
@@ -1,5 +1,44 @@
 package kadachepta;
 
-public class ReviewsService {
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
+@Path("/reviews")
+public class ReviewsService {
+	
+	/**
+	 * kadachepta.com/reviews/artist?name=_name&rating=_rating
+	 * @param artistName
+	 * @param rating
+	 * @return
+	 */
+	@Path("artist")
+	@Produces("text/plain")
+	@Consumes({"text/plain, application/json, application/x-www-form-urlencoded"})
+	public Integer artistRating(
+			@QueryParam("name") String artistName, 
+			@DefaultValue("5") @QueryParam("rating") int rating) {
+		//TODO - save the artist ratings
+		return 0;
+	}
+
+	/**
+	 * kadachepta.com/reviews/story?name=_name&rating=_rating
+	 * @param storyName
+	 * @param rating
+	 * @return
+	 */
+	@Path("story")
+	@Produces("text/plain")
+	@Consumes({"text/plain, application/json, application/x-www-form-urlencoded"})
+	public Integer storyRating(
+			@QueryParam("name") String storyName, 
+			@DefaultValue("5") @QueryParam("rating") String rating) {
+		//TODO - save the story ratings
+		return 0;
+	}
+	
 }
